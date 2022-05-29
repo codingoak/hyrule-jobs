6c54a73c9aef47846a1c5e12c9d8989073381007
Daniel Eicher <d.eicher@mailbox.org>
2022-05-29 12:02:49 +0200

    Use types and props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
+    <JobsList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import JobsList from "./components/JobsList.vue";
 import Job from "./types/Job.js";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobsList },
   setup() {
     const jobs = ref<Job[]>([
       {
---
+++ src/components/JobsList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="job-list">
+    <ul role="list">
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            sint iusto, libero adipisci laudantium accusantium sapiente enim,
+            facere, provident iure animi reprehenderit quidem voluptas culpa
+            magni cum quia! Esse, mollitia!
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Job from "../types/Job.js";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+</style>
